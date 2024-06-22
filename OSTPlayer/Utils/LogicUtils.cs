e1676b88ba9c347e1ce123b2f0dc8096a414db59
ProfeJavix <134125102+ProfeJavix@users.noreply.github.com>
2024-06-22 16:08:05 -0500

    Update LogicUtils.cs

--- OSTPlayer/Utils/LogicUtils.cs
+++ OSTPlayer/Utils/LogicUtils.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Audio;
 using StardewValley;
 using StardewValley.GameData;
 
@@ -6,130 +6,149 @@ namespace OSTPlayer
 {
     public class LogicUtils
     {
-        private static HashSet<string> songNames = new HashSet<string>(){
-            "50s",
-            "AbigailFlute",
-            "AbigailFluteDuet",
-            "aerobics",
-            "archaeo",
-            "bigDrums",
-            "breezy",
-            "caldera",
-            "Cavern",
-            "christmasTheme",
-            "CloudCountry",
-            "ClubLoop",
-            "cowboy_boss",
-            "cowboy_outlawsong",
-            "Cowboy_OVERWORLD",
-            "Cowboy_singing",
-            "Cowboy_undead",
-            "crane_game",
-            "crane_game_fast",
-            "Crystal Bells",
-            "desolate",
-            "distantBanjo",
-            "echos",
-            "elliottPiano",
-            "EmilyDance",
-            "EmilyDream",
-            "EmilyTheme",
-            "end_credits",
-            "event1",
-            "event2",
-            "fall1",
-            "fall2",
-            "fall3",
-            "fallFest",
-            "fieldofficeTentMusic",
-            "FlowerDance",
-            "FrogCave",
-            "Ghost Synth",
-            "grandpas_theme",
-            "gusviolin",
-            "harveys_theme_jazz",
-            "heavy",
-            "honkytonky",
-            "Icicles",
-            "IslandMusic",
-            "jaunty",
-            "junimoKart",
-            "junimoKart_ghostMusic",
-            "junimoKart_mushroomMusic",
-            "junimoKart_slimeMusic",
-            "junimoKart_whaleMusic",
-            "junimoStarSong",
-            "kidadumbautumn",
-            "libraryTheme",
-            "MainTheme",
-            "Majestic",
-            "MarlonsTheme",
-            "marnieShop",
-            "mermaidSong",
-            "moonlightJellies",
-            "movie_classic",
-            "movie_nature",
-            "movie_wumbus",
-            "movieTheater",
-            "movieTheaterAfter",
-            "musicboxsong",
-            "Near The Planet Core",
-            "night_market",
-            "Of Dwarves",
-            "Overcast",
-            "PIRATE_THEME",
-            "playful",
-            "Plums",
-            "poppy",
-            "ragtime",
-            "sad_kid",
-            "sadpiano",
-            "Saloon1",
-            "sam_acoustic1",
-            "sam_acoustic2",
-            "sampractice",
-            "sappypiano",
-            "Secret Gnomes",
-            "SettlingIn",
-            "shaneTheme",
-            "shimmeringbastion",
-            "spaceMusic",
-            "spirits_eve",
-            "spring1",
-            "spring2",
-            "spring3",
-            "springtown",
-            "Stadium_ambient",
-            "starshoot",
-            "submarine_song",
-            "summer1",
-            "summer2",
-            "summer3",
-            "SunRoom",
-            "sweet",
-            "tickTock",
-            "tinymusicbox",
-            "title_night",
-            "tribal",
-            "Tropical Jam",
-            "VolcanoMines1",
-            "VolcanoMines2",
-            "wavy",
-            "wedding",
-            "winter1",
-            "winter2",
-            "winter3",
-            "WizardSong",
-            "woodsTheme",
-            "XOR"
+        private static Dictionary<string, string> gameSongs = new Dictionary<string, string>(){
+            {"MainTheme", "Stardew Valley Overture"},
+            {"title_night", "Load Game"},
+            {"grandpas_theme", "Granpa's Theme"},
+
+            {"spring1", "(Spring) It's A Big World Outside"},
+            {"spring2", "(Spring) The Valley Comes Alive"},
+            {"spring3", "(Spring) Wild Horseradish Jam"},
+            {"summer1", "(Summer) Nature's Crescendo"},
+            {"summer2", "(Summer) The Sun Can Bend An Orange Sky"},
+            {"summer3", "(Summer) Tropicala"},
+            {"fall1", "(Fall) The Smell of Mushroom"},
+            {"fall2", "(Fall) Ghost Synth"},
+            {"fall3", "(Fall) Raven's Descent"},
+            {"winter1", "(Winter) Nocturne Of Ice"},
+            {"winter2", "(Winter) The Wind Can Be Still"},
+            {"winter3", "(Winter) Ancient"},
+
+            {"AbigailFlute", "(Abigail's Melody) A Stillness in the Rain Solo"},
+            {"AbigailFluteDuet", "(Abigail's Melody) A Stillness in the Rain Duet"},
+            {"desolate", "(Alex's Theme) A Sad Song"},
+            {"elliottPiano", "(Elliot's Theme) Piano Solo"},
+            {"EmilyTheme", "(Emily's Theme) Song of Feathers"},
+            {"ragtime", "(Haley's Theme) Pickle Jar Rag"},
+            {"kidadumbautumn", "(Dr. Harvey's Theme) Grapefruit Sky"},
+            {"breezy", "(Leah's Theme) Land of the Green and Gold"},
+            {"spaceMusic", "(Maru's Theme) Starwatcher"},
+            {"50s", "(Penny's Theme) Pleasant Memory"},
+            {"echos", "(Sebastian's Theme) Echos"},
+            {"shaneTheme", "(Shane's Theme) Frozen Pizza and Eggs"},
+            {"WizardSong", "(Wizard's Theme) A Glimpse Of The Other World"},
+
+            {"sampractice", "Band Practice"},
+            {"honkytonky", "Sam's Band (Bluegrass Version)"},
+            {"shimmeringbastion", "Sam's Band (Electronic Version)"},
+            {"heavy", "Sam's Band (Heavy Version)"},
+            {"poppy", "Sam's Band (Pop Version)"},
+            
+            {"junimoKart", "(Junimo Kart) Title Theme"},
+            {"junimoKart_ghostMusic", "(Junimo Kart) Ghastly Galleon"},
+            {"junimoKart_mushroomMusic", "(Junimo Kart) Glowshroom Grotto"},
+            {"junimoKart_slimeMusic", "(Junimo Kart) Slomp's Stomp"},
+            {"junimoKart_whaleMusic", "(Junimo Kart) The Gem Sea Giant"},
+
+            {"Cavern", "(Mines) A Flicker In The Deep"},
+            {"Cloth", "(Mines) Cloth"},
+            {"Crystal Bells", "(Mines) Crystal Bells"},
+            {"tribal", "(Mines) Danger!"},
+            {"Icicles", "(Mines) Icicles"},
+            {"Overcast", "(Mines) Magical Shoes"},
+            {"XOR", "(Mines) Marimba Of Frozen Bones"},
+            {"Secret Gnomes", "(Mines) Star Lumpy"},
+            {"Of Dwarves", "(Mines) The Lava Dwellers"},
+            {"Near The Planet Core", "(Mines) Visitor To The Unknown"},
+            {"VolcanoMines2", "(Volcano Mines) Forgotten World"},
+            {"VolcanoMines1", "(Volcano Mines) Molten Jelly"},
+            
+            {"Cowboy_singing", "(Journey Of The Prairie King) Ending"},
+            {"cowboy_boss", "(Journey Of The Prairie King) Final Boss"},
+            {"Cowboy_OVERWORLD", "(Journey Of The Prairie King) Overworld"},
+            {"cowboy_outlawsong", "(Journey Of The Prairie King) The Outlaw"},
+
+            {"movieTheater", "Movie Theater"},
+            {"movieTheaterAfter", "Movie Theater (Closing Time)"},
+            {"movie_nature", "(Movie Theme) Exploring Our Vibrant World"},
+            {"movie_classic", "(Movie Theme) The Zuzu City Express"},
+            {"movie_wumbus", "(Movie Theme) Wumbus"},
+            
+            {"sadpiano", "A Dark Corner To The Past"},
+            {"junimoStarSong", "A Golden Star Is Born"},
+            {"tinymusicbox", "Alex's Keepsake"},
+            {"sweet", "Buttercup Melody"},
+            {"wavy", "Calico Desert"},
+            {"CloudCountry", "Cloud Country"},
+            {"marnieShop", "Country Shop"},
+            {"crane_game", "Crane Game"},
+            {"crane_game_fast", "Crane Game Fast"},
+            {"moonlightJellies", "Dance of the Moonlight Jellies"},
+            {"distantBanjo", "Distant Banjo"},
+            {"EmilyDream", "Dreamscape"},
+            {"EmilyDance", "Emily's Dance"},
+            {"tickTock", "Festival Game"},
+            {"FlowerDance", "Flower Dance"},
+            {"event1", "Fun Festival"},
+            {"IslandMusic", "Ginger Island"},
+            {"harveys_theme_jazz", "Grapefruit Sky (Pasta Primavera Mix)"},
+            {"woodsTheme", "In The Deep Woods"},
+            {"jaunty", "Jaunty"},
+            {"sad_kid", "Leo's Song"},
+            {"event2", "Luau Festival"},
+            {"mermaidSong", "Mermaid Song"},
+            {"musicboxsong", "Music Box Song"},
+            {"caldera", "Mystery Of The Caldera"},
+            {"night_market", "Night Market"},
+            {"springtown", "Pelican Town"},
+            {"PIRATE_THEME", "Pirate Theme"},
+            {"playful", "Playful"},
+            {"fieldofficeTentMusic", "Professor Snail's Radio"},
+            {"fallFest", "Stardew Valley Fair Theme"},
+            {"Saloon1", "Stardrop Saloon"},
+            {"MarlonsTheme", "The Adventure Guild"},
+            {"FrogCave", "The Gourmand's Cave"},
+            {"sam_acoustic1", "The Happy Junimo Show"},
+            {"libraryTheme", "The Library And Museum"},
+            {"SettlingIn", "Settling In"},
+            {"spirits_eve", "Spirit's Eve Festival"},
+            {"submarine_song", "Submarine Theme"},
+            {"SunRoom", "Sun Room (Alone With Relaxing Tea)"},
+            {"gusviolin", "Violin Solo"},
+            {"wedding", "Wedding Celebration"},
+            {"christmasTheme", "Winter Festival"},
+
+            {"end_credits", "(Credits) Summit Celebration"},
+
+            {"archaeo", null},
+            {"Cowboy_undead", null},
+            {"sappypiano", null},
+            {"starshoot", null}
         };
         public static List<Song> GetAllSongs()
         {
             List<Song> songs = new List<Song>();
-            foreach(string name in songNames){
-                songs.Add(new Song(name));
+            foreach(KeyValuePair<string, string> item in gameSongs){
+                songs.Add(new Song(item.Key, item.Value));
             }
             return songs;
+        }
+
+        public static int CompareSongsByPos(Song s1, Song s2){
+
+            if(s1.HasName && !s2.HasName)
+                return 1;
+            if(!s1.HasName && s2.HasName)
+                return -1;
+
+            foreach(KeyValuePair<string, string> item in gameSongs){
+                if(item.Key == s1.Id)
+                    return 1;
+                if(item.Key == s2.Id)
+                    return -1;
+            }
+
+            return 0;
         }
 
         // Removes song added by default while playing a track
