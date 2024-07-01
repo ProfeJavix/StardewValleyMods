4d03dc1e385a59e23649a09cc82e1667472fa4b1
ProfeJavix <134125102+ProfeJavix@users.noreply.github.com>
2024-07-01 11:54:09 -0500

    Update IUIHelperApi.cs

--- UIHelper/IUIHelperApi.cs
+++ UIHelper/IUIHelperApi.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using StardewValley.Menus;
 
-namespace UIHelper
+namespace //YourWorkspaceName
 {
     public interface IUIHelperApi
     {
